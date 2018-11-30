ff1914efce1b9b9b930046e1c33544ae6238b9a5
Yahyarashid <yahyomar63@gmail.com>
2018-11-30 12:55:00 +0200

    jjjj

--- server.js
+++ server.js
@@ -22,13 +22,6 @@ app.get('/', function (req, res) {
 
 		
 })
-	if(process.env.NODE_ENV === 'production'){
-    //set static folder
-    app.use(express.static('client/build'));
-}
-app.get('*',(req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
 
 
 	//res.send('hello iam there')
